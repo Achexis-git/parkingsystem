aeeb371223ad19599bd9957e21d0b22a035ef017
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-11-14 18:14:55 +0100

    Ajout du test de réduction

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,10 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -16,11 +18,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
 
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -31,10 +35,11 @@ public class ParkingDataBaseIT {
     private static DataBasePrepareService dataBasePrepareService;
     
     private Ticket ticket;
+    private static FareCalculatorService fareCalculatorService;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
-
+    
     @BeforeAll
     private static void setUp() throws Exception{
         parkingSpotDAO = new ParkingSpotDAO();
@@ -49,6 +54,7 @@ public class ParkingDataBaseIT {
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
+        ticket = new Ticket();
     }
 
     @AfterAll
@@ -87,6 +93,30 @@ public class ParkingDataBaseIT {
         assertThat(ticket.getPrice()).isCloseTo(0.0, within(0.5));
         // Vérifie que l'heure de sortie est proche de l'heure actuelle (et donc existe)
         assertThat(ticket.getOutTime()).isCloseTo(new Date(), TimeUnit.SECONDS.toMillis(5));
+    }
+    
+    @Test
+    public void testReccurentUserReduction() {
+    	fareCalculatorService = new FareCalculatorService();
+    	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+        parkingService.processExitingVehicle();
+        
+        Date inTime = new Date();
+		inTime.setTime(System.currentTimeMillis() - (10 * 60 * 60 * 1000));// 10h parking time
+		
+		Date outTime = new Date();
+		
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		
+		ticket.setInTime(inTime);
+		ticket.setOutTime(outTime);
+		ticket.setParkingSpot(parkingSpot);
+		ticket.setVehicleRegNumber("ABCDEF");
+		fareCalculatorService.calculateFare(ticket);
+        
+        
+        assertThat(ticket.getPrice()).isCloseTo(10 * Fare.CAR_RATE_PER_HOUR * 0.95, within(0.01)); // 95% du prix
     }
 
 }
