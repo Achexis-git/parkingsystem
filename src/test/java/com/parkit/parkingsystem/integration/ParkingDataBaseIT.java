ad1868f9ab45d4c49f9c978348367ffa07ac1b4d
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-11-17 17:38:59 +0100

    Formattage

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import static org.mockito.Mockito.verify;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
